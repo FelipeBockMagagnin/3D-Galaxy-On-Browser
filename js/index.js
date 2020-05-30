e88273c744a5b25c980c2d0f65a1c129370043a6
Felpi <felipebmagagnin@gmail.com>
2020-05-30 14:48:41 -0300

    add jupiter, mars and refactor code

--- js/index.js
+++ js/index.js
@@ -1,104 +1,38 @@
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+camera.position.z = 30;
 
-var mercury = generateMercury();
-mercury.position.x = 2;
-scene.add(mercury);
+var planets = generatePlanets();
 
-var sun = generateSun();
-scene.add(sun);
-
-var venus = generateVenus();
-venus.position.x = 4;
-scene.add(venus);
-
-var earth = generateEarth();
-scene.add(earth);
+planets.forEach(element => {
+  scene.add(element.object);
+});
 
 animate();
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  mercury.rotation.x += 0.01;
-  mercury.rotation.y += 0.01;
 
-  sun.rotation.x += 0.01;
-  sun.rotation.y += 0.01;
-
-  venus.rotation.x += 0.02;
-  venus.rotation.y += 0.02;
-
-  //earth.rotation.x += 0.03;
-  //earth.rotation.y += 0.03;
-  //camera.position.z += 0.003;
-  a = (a + Math.PI / 360) % (Math.PI * 2);
-  rotate(a);
-  //orbitBody(earth);
+  planets.forEach(element => {
+    rotate(element);
+  });
 }
 
-function generateMercury() {
-  var geometry = new THREE.SphereGeometry(0.5, 32, 32)
-  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/mercury/mercurymap.jpg')
-  material.bumpMap = THREE.ImageUtils.loadTexture('images/mercury/mercurybump.jpg')
-  material.bumpScale = 0.05
-  return new THREE.Mesh(geometry, material);
+function rotate(planetObj) {
+  var px = planetObj.radius * Math.cos(planetObj.angle); // <-- that's the maths you need
+  var py = planetObj.radius * Math.sin(planetObj.angle);
+
+  planetObj.angle = (planetObj.angle + Math.PI / 360) % (Math.PI * 2) + planetObj.translationSpeed;
+  planetObj.object.position.x = px;
+  planetObj.object.position.y = py;
+
+  //planetObj.object.rotation.x += planetObj.rotationSpeed
+  planetObj.object.rotation.y += planetObj.rotationSpeed
 }
-
-function generateSun() {
-  var geometry = new THREE.SphereGeometry(1, 32, 32)
-  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/sun/sunmap.jpg')
-  return new THREE.Mesh(geometry, material);
-}
-
-function generateVenus() {
-  var geometry = new THREE.SphereGeometry(0.5, 32, 32)
-  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/venus/venusbump.jpg')
-  material.bumpMap = THREE.ImageUtils.loadTexture('images/venus/venusmap.jpg')
-  material.bumpScale = 0.05
-  return new THREE.Mesh(geometry, material);
-}
-
-function generateEarth() {
-  var geometry = new THREE.SphereGeometry(0.5, 64, 64)
-  var material = new THREE.MeshBasicMaterial()
-  material.map = THREE.ImageUtils.loadTexture('images/earth/earthmap1k.jpg')
-  material.bumpMap = THREE.ImageUtils.loadTexture('images/earth/earthbump1k.jpg')
-  material.bumpScale = 0.05
-  material.specularMap = THREE.ImageUtils.loadTexture('images/earth/earthspec1k.jpg')
-  material.specular = new THREE.Color('grey')
-  return new THREE.Mesh(geometry, material)
-}
-
-
-var x = 100  // center
-var y = 50   // center
-var r = 50   // radius
-var a = 0    // angle (from 0 to Math.PI * 2)
-
-function rotate(a) {
-  
-  var px = x + r * Math.cos(a); // <-- that's the maths you need
-  var py = y + r * Math.sin(a);
-  console.log(px);
-  console.log(py);
-
-  earth.position.x = px / 20 - 5;
-  earth.position.y = py / 20 - 2;
-  
-}
-
-
-setInterval(function() {
-  a = (a + Math.PI / 360) % (Math.PI * 2);
-  rotate(a);
-}, 5);
---
+++ js/planets.js
@@ -0,0 +1,134 @@
+class planet {
+  name
+  radius
+  rotationSpeed
+  translationSpeed
+  object
+  angle
+}
+
+function generatePlanets(){
+  let planets = [];
+
+  planets.push(generateMercury())
+  planets.push(generateSun())
+  planets.push(generateVenus())
+  planets.push(generateEarth())
+  planets.push(generateMars())
+  planets.push(generateJupiter())
+  planets.push(generateEarth())
+
+  return planets;
+}
+
+function generateSun() {
+  var geometry = new THREE.SphereGeometry(2, 32, 32)
+  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  material.map = THREE.ImageUtils.loadTexture('images/sun/sunmap.jpg')
+
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Sun',
+    radius: 0,
+    rotationSpeed: .01,
+    translationSpeed: 0,
+    object: planet,
+    angle: 0
+  }
+}
+
+function generateMercury() {
+  var geometry = new THREE.SphereGeometry(0.3, 32, 32)
+  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  material.map = THREE.ImageUtils.loadTexture('images/mercury/mercurymap.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/mercury/mercurybump.jpg')
+  material.bumpScale = 0.05
+  
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Mercury',
+    radius: 4,
+    rotationSpeed: .01,
+    translationSpeed: .008,
+    object: planet,
+    angle: Math.random(0, Math.PI * 2)
+  }
+}
+
+function generateVenus() {
+  var geometry = new THREE.SphereGeometry(0.4, 32, 32)
+  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  material.map = THREE.ImageUtils.loadTexture('images/venus/venusbump.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/venus/venusmap.jpg')
+  material.bumpScale = 0.05
+
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Venus',
+    radius: 6,
+    rotationSpeed: .009,
+    translationSpeed: .003,
+    object: planet,
+    angle: Math.random(0, Math.PI * 2)
+  }
+}
+
+function generateEarth() {
+  var geometry = new THREE.SphereGeometry(0.6, 64, 64)
+  var material = new THREE.MeshBasicMaterial()
+  material.map = THREE.ImageUtils.loadTexture('images/earth/earthmap1k.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/earth/earthbump1k.jpg')
+  material.bumpScale = 0.05
+  material.specularMap = THREE.ImageUtils.loadTexture('images/earth/earthspec1k.jpg')
+  material.specular = new THREE.Color('grey')
+
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Earth',
+    radius: 10,
+    rotationSpeed: .02,
+    translationSpeed: -0.005,
+    object: planet,
+    angle: Math.random(0, Math.PI * 2)
+  }  
+}
+
+function generateMars() {
+  var geometry = new THREE.SphereGeometry(0.8, 64, 64)
+  var material = new THREE.MeshBasicMaterial()
+  material.map = THREE.ImageUtils.loadTexture('images/mars/mars_1k_color.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/mars/marsbump1k.jpg')
+  material.bumpScale = 0.05
+
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Mars',
+    radius: 15,
+    rotationSpeed: .01,
+    translationSpeed: -0.007,
+    object: planet,
+    angle: Math.random(0, Math.PI * 2)
+  }
+}
+
+function generateJupiter() {
+  var geometry = new THREE.SphereGeometry(1.7, 64, 64)
+  var material = new THREE.MeshBasicMaterial()
+  material.map = THREE.ImageUtils.loadTexture('images/jupiter/jupitermap.jpg')
+
+  let planet = new THREE.Mesh(geometry, material);
+
+  return {
+    name: 'Earth',
+    radius: 22,
+    rotationSpeed: .008,
+    translationSpeed: -0.008,
+    object: planet,
+    angle: Math.random(0, Math.PI * 2)
+  }
+}
