ffc9a253fea49fbabc3c2ccb152e7c8ab5489a7b
Felpi <felipebmagagnin@gmail.com>
2020-05-30 11:05:36 -0300

    add sun and venus

--- js/index.js
+++ js/index.js
@@ -8,7 +8,15 @@ document.body.appendChild(renderer.domElement);
 camera.position.z = 5;
 
 var mercury = generateMercury();
+mercury.position.x = 2;
 scene.add(mercury);
+
+var sun = generateSun();
+scene.add(sun);
+
+var venus = generateVenus();
+venus.position.x = 4;
+scene.add(venus);
 
 animate();
 
@@ -17,7 +25,13 @@ function animate() {
   renderer.render(scene, camera);
   mercury.rotation.x += 0.01;
   mercury.rotation.y += 0.01;
-  camera.position.z += 0.003;
+
+  sun.rotation.x += 0.01;
+  sun.rotation.y += 0.01;
+
+  venus.rotation.x += 0.02;
+  venus.rotation.y += 0.02;
+  //camera.position.z += 0.003;
 }
 
 function generateMercury(){
@@ -30,9 +44,18 @@ function generateMercury(){
 }
 
 function generateSun(){
-  var geometry = new THREE.SphereGeometry(0.5, 64, 64)
+  var geometry = new THREE.SphereGeometry(1, 64, 64)
   var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
   material.map = THREE.ImageUtils.loadTexture('images/sunmap.jpg')
+  return new THREE.Mesh(geometry, material);
+}
+
+function generateVenus(){
+  var geometry = new THREE.SphereGeometry(0.5, 32, 32)
+  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  material.map = THREE.ImageUtils.loadTexture('images/venusbump.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/venusmap.jpg')
+  material.bumpScale = 0.05
   return new THREE.Mesh(geometry, material);
 }
 
