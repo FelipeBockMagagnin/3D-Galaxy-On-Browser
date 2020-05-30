c54fba999f8426251f076583e06f5f66871541f6
Felpi <felipebmagagnin@gmail.com>
2020-05-30 11:19:54 -0300

    add earth

--- js/index.js
+++ js/index.js
@@ -18,6 +18,10 @@ var venus = generateVenus();
 venus.position.x = 4;
 scene.add(venus);
 
+var earth = generateEarth();
+earth.position.x = 6;
+scene.add(earth);
+
 animate();
 
 function animate() {
@@ -31,31 +35,45 @@ function animate() {
 
   venus.rotation.x += 0.02;
   venus.rotation.y += 0.02;
+
+  earth.rotation.x += 0.03;
+  earth.rotation.y += 0.03;
   //camera.position.z += 0.003;
 }
 
-function generateMercury(){
+function generateMercury() {
   var geometry = new THREE.SphereGeometry(0.5, 32, 32)
   var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/mercurymap.jpg')
-  material.bumpMap = THREE.ImageUtils.loadTexture('images/mercurybump.jpg')
+  material.map = THREE.ImageUtils.loadTexture('images/mercury/mercurymap.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/mercury/mercurybump.jpg')
   material.bumpScale = 0.05
   return new THREE.Mesh(geometry, material);
 }
 
-function generateSun(){
-  var geometry = new THREE.SphereGeometry(1, 64, 64)
+function generateSun() {
+  var geometry = new THREE.SphereGeometry(1, 32, 32)
   var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/sunmap.jpg')
+  material.map = THREE.ImageUtils.loadTexture('images/sun/sunmap.jpg')
   return new THREE.Mesh(geometry, material);
 }
 
-function generateVenus(){
+function generateVenus() {
   var geometry = new THREE.SphereGeometry(0.5, 32, 32)
   var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  material.map = THREE.ImageUtils.loadTexture('images/venusbump.jpg')
-  material.bumpMap = THREE.ImageUtils.loadTexture('images/venusmap.jpg')
+  material.map = THREE.ImageUtils.loadTexture('images/venus/venusbump.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/venus/venusmap.jpg')
   material.bumpScale = 0.05
   return new THREE.Mesh(geometry, material);
+}
+
+function generateEarth() {
+  var geometry = new THREE.SphereGeometry(0.5, 64, 64)
+  var material = new THREE.MeshBasicMaterial()
+  material.map = THREE.ImageUtils.loadTexture('images/earth/earthmap1k.jpg')
+  material.bumpMap = THREE.ImageUtils.loadTexture('images/earth/earthbump1k.jpg')
+  material.bumpScale = 0.05
+  material.specularMap = THREE.ImageUtils.loadTexture('images/earth/earthspec1k.jpg')
+  material.specular = new THREE.Color('grey')
+  return new THREE.Mesh(geometry, material)
 }
 
